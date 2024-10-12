c9f85b22749ff9c5059372d75141b945c3d41cbc
TeaJhay <140999577+TeaJhay@users.noreply.github.com>
2024-10-12 13:37:18 +0000

    Update theme.sh

--- install/includes/theme.sh
+++ install/includes/theme.sh
@@ -3,27 +3,36 @@ theme_files=(
     ".gtkrc-2.0"
     ".icons/default/index.theme"
     ".config/xsettingsd/xsettingsd.conf"
+)
 
 echo -e "${GREEN}"
 figlet "Theme"
 echo -e "${NONE}"
 echo "The script can set dark mode setting through gsettings"
-if gum confirm "Do you want to create a backup of theme files now" ;then
 
-  #create backup folder
-  if [ ! -d ~/ml4w/backup/themes ] ;then
-      mkdir ~/ml4w/backup/themes
-  fi
+if gum confirm "Do you want to create a backup of theme files now"; then
 
-  for tf in "${theme_files[@]}"
-  do
-    if [ -d ~/$tf ] ;then
-      echo "::backup of $tf"
-      cp -r ~/$tf ~/ml4w/backup/themes/$tf
+    # Create backup folder
+    if [ ! -d ~/ml4w/backup/themes ]; then
+        mkdir -p ~/ml4w/backup/themes
     fi
-echo "Backup done"
-echo "running commands"
-  gsettings set $gnome-schema gtk-theme 'Adwaita-dark'
-  gsettings set $gnome-schema icon-theme 'Breeze'
-  gsettings set $gnome-schema cursor-theme 'Oxygen Zion'
-echo "Done!"
+
+    for tf in "${theme_files[@]}"; do
+        if [ -d "$HOME/$tf" ]; then
+            echo "::backup of $tf"
+            cp -r "$HOME/$tf" "$HOME/ml4w/backup/themes/$tf"
+        fi
+    done
+
+    echo "Backup done"
+    echo "running commands"
+# Set the GNOME schema
+gnome_schema="org.gnome.desktop.interface"
+
+# Apply the settings
+gsettings set "$gnome_schema" gtk-theme 'Breeze-Dark'
+gsettings set "$gnome_schema" icon-theme 'breeze-dark'
+gsettings set "$gnome_schema" cursor-theme 'Oxygen_Zion'
+    
+    echo "Done!"
+fi
